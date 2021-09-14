154e8a71fdab51afc3c7d1b3195b5a3206e9024a
Adaml <adam6925@stud.kea.dk>
2021-09-14 14:31:09 +0200

    can convert a single numberToLetter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,13 @@ public class Main {
         /*char letter = 'E' ;
         int number = letterToNumber(letter);
         System.out.printf("Bogstaver %c bliver til %d", letter, number);*/
-        String plainText = "ADAM";
+        /*String plainText = "ADAM";
         int[] cipher = textToListOfNumbers(plainText);
 
-        System.out.println(Arrays.toString(cipher));
+        System.out.println(Arrays.toString(cipher));*/
+        int number = 5;
+        char letter = numberToLetter( number );
+        System.out.printf("Tallet %d bliver til bogstavet %c", number, letter);
     }
     public static int[] textToListOfNumbers(String text){
      int[] numbers = new int[text.length()];
@@ -27,6 +30,11 @@ public class Main {
         int num = alfabetet.indexOf( letter );
         return num;
 
+    }
+    public static char numberToLetter( int number ){
+        String alfabetet = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+        char letter = alfabetet.charAt(number);
+        return letter;
     }
 
 }
