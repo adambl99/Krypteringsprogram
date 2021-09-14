fd41e0e7c1b2e5c80a7a1a041194af0f31e4e5f0
Adaml <adam6925@stud.kea.dk>
2021-09-14 14:44:01 +0200

    can convert a single numberToLetter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,9 +12,14 @@ public class Main {
         int[] cipher = textToListOfNumbers(plainText);
 
         System.out.println(Arrays.toString(cipher));*/
-        int number = 5;
+        /*int number = 5;
         char letter = numberToLetter( number );
-        System.out.printf("Tallet %d bliver til bogstavet %c", number, letter);
+        System.out.printf("Tallet %d bliver til bogstavet %c", number, letter);*/
+
+        int[] cipher = {1, 2, 5};
+        String plainText = listofNumbersToText(cipher);
+        System.out.println("Den decryptede tekst er: ");
+        System.out.println(plainText);
     }
     public static int[] textToListOfNumbers(String text){
      int[] numbers = new int[text.length()];
@@ -31,10 +36,22 @@ public class Main {
         return num;
 
     }
+    public static String listofNumbersToText ( int[] numbers) {
+        String text = "";
+        for (int i = 0; i < numbers.length; ++i) {
+            int number = numbers[i];
+
+            char letter = numberToLetter(number);
+
+            text = text + letter;
+        }
+        return text;
+    }
     public static char numberToLetter( int number ){
         String alfabetet = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
         char letter = alfabetet.charAt(number);
         return letter;
     }
+
 
 }
